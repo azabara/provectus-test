bb1258474337764c2575f1c05420ddb322b53928
azabara <46056342+azabara@users.noreply.github.com>
2021-07-07 14:34:05 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   cidr = "172.31.0.0/16"
 
   azs             = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
-  private_subnets = ["172.31.0.0/16"]
-  public_subnets  = ["172.31.0.0/24"]
+  private_subnets = ["172.31.0.1/24", "172.31.0.2/24", "172.31.0.3/24"]
+  public_subnets  = ["172.31.101.0/24", "172.31.102.0/24", "172.31.103.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
