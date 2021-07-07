cb4e507f73cd7546abb9f9f1786e7e91d8a65ba0
azabara <46056342+azabara@users.noreply.github.com>
2021-07-07 14:43:40 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "provectus"
-  cidr = "172.31.0.0/16"
+  cidr = "10.0.0.0/16"
 
   azs             = ["eu-central-1a", "eu-centralest-1b", "eu-central-1c"]
-  private_subnets = ["172.31.0.1/24", "172.31.0.2/24", "172.31.0.3/24"]
-  public_subnets  = ["172.31.101.0/24", "172.31.102.0/24", "172.31.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = false
