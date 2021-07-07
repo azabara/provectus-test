4692dd5787692472d2cc7abc96e75978724192ff
azabara <46056342+azabara@users.noreply.github.com>
2021-07-07 14:41:44 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "provectus"
   cidr = "172.31.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  azs             = ["eu-central-1a", "eu-centralest-1b", "eu-central-1c"]
   private_subnets = ["172.31.0.1/24", "172.31.0.2/24", "172.31.0.3/24"]
   public_subnets  = ["172.31.101.0/24", "172.31.102.0/24", "172.31.103.0/24"]
 
