cf38f3de8eb2380b95b7d2cbaf93429029a07033
azabara <46056342+azabara@users.noreply.github.com>
2021-07-07 14:37:32 +0300

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -9,10 +9,22 @@ module "vpc" {
   public_subnets  = ["172.31.101.0/24", "172.31.102.0/24", "172.31.103.0/24"]
 
   enable_nat_gateway = true
+  single_nat_gateway = false
+  one_nat_gateway_per_az = false
   enable_vpn_gateway = true
+  reuse_nat_ips       = true                    # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = "${aws_eip.nat.*.id}"   # <= IPs specified here as input to the module
+
 
   tags = {
     Terraform = "true"
     Environment = "dev"
   }
+}
+
+  
+resource "aws_eip" "nat" {
+  count = 3
+
+  vpc = true
 }
