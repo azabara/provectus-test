5e4d55ebcdf67cbb963a843daa9ccd50c67ee77a
azabara <46056342+azabara@users.noreply.github.com>
2021-07-08 23:20:02 +0300

    Delete backend_s3.tf

--- backend_s3.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "bucket-of-tulips"
-    key    = "global/s3/terrafrom.tfstate"
-    region = "eu-central-1"
-    dynamodb_table = "tf-be"
-    encrypt = "true"
-  }
-}
