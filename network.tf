e37de816c8c7d0f8e08fcbdaa9540bc85aaec64b
azabara <46056342+azabara@users.noreply.github.com>
2021-07-09 01:24:32 +0300

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
- resource "aws_vpc" "Main" {                # Creating VPC here
-   cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
+ resource "aws_vpc" "provectus" {                # Creating VPC here
+   cidr_block       = var.provectus_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
    instance_tenancy = "default"
   
   tags = {
@@ -8,21 +8,21 @@
  }
 
  resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway
-    vpc_id =  aws_vpc.Main.id               # vpc_id will be generated after we create VPC
+    vpc_id =  aws_vpc.provectus.id               # vpc_id will be generated after we create VPC
  }
 
 resource "aws_subnet" "publicsubnets" {    # Creating Public Subnets
-   vpc_id =  aws_vpc.Main.id
+   vpc_id =  aws_vpc.provectus.id
    cidr_block = "${var.public_subnets}"        # CIDR block of public subnets
  }
 
  resource "aws_subnet" "privatesubnets" {
-   vpc_id =  aws_vpc.Main.id
+   vpc_id =  aws_vpc.provectus.id
    cidr_block = "${var.private_subnets}"          # CIDR block of private subnets
  }
  
  resource "aws_route_table" "PublicRT" {    # Creating RT for Public Subnet
-    vpc_id =  aws_vpc.Main.id
+    vpc_id =  aws_vpc.provectus.id
          route {
     cidr_block = "0.0.0.0/0"               # Traffic from Public Subnet reaches Internet via Internet Gateway
     gateway_id = aws_internet_gateway.IGW.id
@@ -30,7 +30,7 @@ resource "aws_subnet" "publicsubnets" {    # Creating Public Subnets
  }
  
  resource "aws_route_table" "PrivateRT" {    # Creating RT for Private Subnet
-   vpc_id = aws_vpc.Main.id
+   vpc_id = aws_vpc.provectus.id
    route {
    cidr_block = "0.0.0.0/0"             # Traffic from Private Subnet reaches Internet via NAT Gateway
    nat_gateway_id = aws_nat_gateway.NATgw.id
